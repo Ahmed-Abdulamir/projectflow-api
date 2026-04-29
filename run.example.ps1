7ce5c145fb6e086ad0f165cddb7f96ba4634f13a
Ahmed-Abdulamir <ahmesabdulamir@gmail.com>
2026-04-29 16:41:32 +0300

    Add Docker Compose for PostgreSQL

--- run.example.ps1
+++ run.example.ps1
@@ -2,7 +2,7 @@ $env:PROJECTFLOW_DB_HOST = "127.0.0.1"
 $env:PROJECTFLOW_DB_PORT = "5432"
 $env:PROJECTFLOW_DB_NAME = "task_manager_db"
 $env:PROJECTFLOW_DB_USER = "postgres"
-$env:PROJECTFLOW_DB_PASSWORD = "your_postgres_password"
+$env:PROJECTFLOW_DB_PASSWORD = "postgres"
 $env:PROJECTFLOW_SERVER_PORT = "8080"
 
 Set-Location "C:\Projects\Task Manager API\build"
